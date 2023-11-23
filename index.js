b8c3546607f5527b3ed3732cc64e7303028d3e3d
Andres Correa <andrescorreac@gmail.com>
2023-11-23 09:20:24 -0500

    Merge Sort Practice

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+function mergeSort(toSort){
+    if(toSort.length <= 1){return toSort}
+    console.log("ToSort", toSort)
+    let middle = Math.floor((toSort.length)/2)
+    let left =  mergeSort(toSort.slice(0, middle)) 
+    let right = mergeSort(toSort.slice(middle)) 
+    return merge(left,right)
+} 
+
+console.log("FINAL ANSWER",mergeSort([9, 8, 7, 6,7, 5, 4, 3, 2, 1]))
+
+function merge(a, b, m = 0, n = 0, result = []){
+    while((a.length > m) && (b.length > n)){
+        if(a[m] <= b[n]) { result.push(a[m++])}
+        if(b[n] <= a[m]) { result.push(b[n++])}
+    }
+    for (; a.length > m ; m++) {
+        result.push(a[m])
+    }
+    for (; b.length > n; n++) {
+        result.push(b[n])
+    }
+    return result 
+}
+
+// console.log(merge([2,5,6],[3,4,7]))
+
+
+
+
+// --------------- Excercise N1 -------------
 
 // function fibs(num){
 //     let first = 0
@@ -15,13 +46,13 @@
 //     return result
 // }
 
-function fibsRec(num, result = [0,1], first = 0, second = 1 ){
-    if (result.length === num){return result}
-    if (result.length < num){
-        partial = first + second
-        result.push(partial)
-        return fibsRec(num, result, first = second, second = partial)
-    }
-}
+// function fibsRec(num, result = [0,1], first = 0, second = 1 ){
+//     if (result.length === num){return result}
+//     if (result.length < num){
+//         partial = first + second
+//         result.push(partial)
+//         return fibsRec(num, result, first = second, second = partial)
+//     }
+// }
 
-console.log(fibsRec(8)) // [0, 1, 1, 2, 3, 5, 8, 13]
+// console.log(fibsRec(8)) // [0, 1, 1, 2, 3, 5, 8, 13]
